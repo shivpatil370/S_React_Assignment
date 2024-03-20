1e665b8d6c1d6677b80dcb150a3316ad2bd9bf39
Amit Deshmukh <shivpatil308@gmail.com>
2024-03-20 16:58:24 +0530

    total cart Items added

--- Creating_a_Restaurant_Website/shopping-app/src/components/Layout/HeaderCartButton.js
+++ Creating_a_Restaurant_Website/shopping-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon"
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+   const cartCtx=useContext(CartContext);
+
+   const numberOfCartItems=cartCtx.items.reduce((acc,items)=>{
+    return acc+items.amount;
+   },0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
        <span className={styles.icon}>
         <CartIcon/>
        </span>
        <span>Your Cart</span>
-       <span className={styles.badge}>3</span>
+       <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
