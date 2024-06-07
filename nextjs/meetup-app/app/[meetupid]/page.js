9e6ec8a7ce395e3a09161effb42bc2a3443b2cad
Amit Deshmukh <shivpatil308@gmail.com>
2024-06-07 20:48:01 +0530

    detail page done

--- nextjs/meetup-app/app/[meetupid]/page.js
+++ nextjs/meetup-app/app/[meetupid]/page.js
@@ -1,16 +1,37 @@
+"use client";
 import MeetupDetails from "@/components/meetups/MeetupDetails";
+// import { useState } from "react";
+// import { getData } from "../page";
 
 
 
 
-export default function MeetupDetail(){
+export default async function MeetupDetail({params}){
+    // const [data,setData]=useState([]);
+    let param=params.meetupid;
+    // console.log(param);
+
+   
+   let x =await getData();
+
+    // console.log(x);
+
+    const filter=x.find((ele)=>param==ele.id)
 
     return(
         <MeetupDetails
-        image="https://my.alfred.edu/zoom/_images/foster-lake.jpg"
-        title="first meetup!"
-        address="some street 5"
-        description="this is first meetup"
+        image={filter.image}
+        title={filter.title}
+        address={filter.address}
+        description={filter.description}
         />
     )
 }
+
+export async function getData(){
+    const response=await fetch("http://localhost:8080/DUMMY_MEETUPS");
+    const data=await response.json();
+    // setData(data);
+    return data;
+    // console.log(datas)
+};
