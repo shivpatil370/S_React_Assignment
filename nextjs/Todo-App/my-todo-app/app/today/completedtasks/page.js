700d16f8ad9369355379338c4ab6423118f4c6cc
Amit Deshmukh <shivpatil308@gmail.com>
2024-06-09 16:49:19 +0530

    app router added

---
+++ nextjs/Todo-App/my-todo-app/app/today/completedtasks/page.js
@@ -0,0 +1,11 @@
+
+
+
+export default function CompletedTasks(){
+
+    return(
+        <>
+        <h1>Completed Tasks!</h1>
+        </>
+    )
+}
---
+++ nextjs/Todo-App/my-todo-app/app/today/page.js
@@ -0,0 +1,13 @@
+
+
+export default function Today_Task(){
+
+    return(
+        <>
+        <span><h1>Today</h1> <p>Sat/20-may</p></span>
+        <input/>
+        <button>cancel</button>
+        <button>add</button>
+        </>
+    )
+}
